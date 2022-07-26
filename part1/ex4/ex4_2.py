fcc1741cf21b00daaba806157e62efb0c3f17355
Sharry Xu <sharry.xu@outlook.com>
2022-07-26 14:34:18 +0800

    Keep learning.

--- part1/ex4/ex4_2.py
+++ part1/ex4/ex4_2.py
@@ -1,2 +1,16 @@
+def calc_lines(name: str):
+    length = len(name)
+    if length == 0:
+        return 0
+    line_count = 1
+    while length > 1:
+        length -= line_count
+        line_count += 1
+    return line_count
+
+
 def print_name(name: str):
-    pass
+    line_count = calc_lines(name)
+    max_line_length = (line_count) - 1 * 2
+    for i in range(line_count + 1):
+        pass
---
+++ part1/ex4/test_ex4_2.py
@@ -0,0 +1,6 @@
+from ex4_2 import calc_lines
+
+
+class TestEx4_2:
+    def test(self):
+        assert calc_lines("12345") == 3
